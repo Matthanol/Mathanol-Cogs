06b34abc437ee86f261996e23d61e7b8c655cc8d
Matthanol <matthias.bruynooghe@gmail.com>
2021-06-19 10:05:37 +0200

    added comments

--- counter/counter.py
+++ counter/counter.py
@@ -18,7 +18,7 @@ class Counter(commands.Cog):
 
     @commands.command()
     async def count(self, ctx, name, increment = 1):
-
+        """Add an amount to a counter, if you don't specify an amount the default is adding 1"""
         async with self.config.guild(ctx.guild).counters() as counters:
             if counters.get(name) == None:
                 counters[name] = 0
@@ -26,12 +26,14 @@ class Counter(commands.Cog):
             await ctx.send("current count for {} is {}".format(name, counters[name]))
     @commands.command()
     async def resetCount(self, ctx, name):
+        """Sets count back to 0"""
         async with self.config.guild(ctx.guild).counters() as counters:
             counters[name] = 0
             await ctx.send("the counter for {} has been set back to 0".format(name))
 
     @commands.command()
     async def setCount(self, ctx, name, amount):
+        """Set the count to a certain amount"""
         async with self.config.guild(ctx.guild).counters() as counters:
             counters[name] = amount
             await ctx.send("current count for {} is {}".format(name, counters[name]))
